f211650ed4cbe2866d126a7f6090b4085dcfe9b1
Frieder Singer <117061955+friedersinger@users.noreply.github.com>
2022-11-11 15:10:39 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,107 @@
+'use strict';
+
+// Selecting elements
+const player0EL = document.querySelector(`.player--0`);
+const player1EL = document.querySelector(`.player--1`);
+const score0El = document.querySelector(`#score--0`);
+// a little bit faster as querySelector
+const score1El = document.getElementById(`score--1`);
+const current0El = document.getElementById(`current--0`);
+const current1El = document.getElementById(`current--1`);
+
+const diceEl = document.querySelector(`.dice`);
+const btnNew = document.querySelector(`.btn--new`);
+const btnRoll = document.querySelector(`.btn--roll`);
+const btnHold = document.querySelector(`.btn--hold`);
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  // if playing isn't true, the buttons are deactived
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add(`hidden`);
+  player0EL.classList.remove(`player--winner`);
+  player1EL.classList.remove(`player--winner`);
+  player0EL.classList.add(`player--active`);
+  player1EL.classList.remove(`player--active`);
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0EL.classList.toggle(`player--active`);
+  player1EL.classList.toggle(`player--active`);
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener(`click`, function () {
+  // if playing is true
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove(`hidden`);
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // switch to next player
+      // all down in a function
+      // document.getElementById(`current--${activePlayer}`).textContent = 0;
+      // currentScore = 0;
+      // activePlayer = activePlayer === 0 ? 1 : 0;
+      // // toggle will only put the class player--active in the player Element
+      // // if it's missing, and removes it's if it's already inside
+      // player0EL.classList.toggle(`player--active`);
+      // player1EL.classList.toggle(`player--active`);
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener(`click`, function () {
+  if (playing) {
+    console.log(`Hold button`);
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+
+    //   scores[1] = scores [1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      // var playing now getting false
+      playing = false;
+      // dice should disappear
+      diceEl.classList.add(`hidden`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener(`click`, init);
